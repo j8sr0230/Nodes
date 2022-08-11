3f7381950e41919e93f4a33b68af92838eb2305d
j8sr0230 <ronny.scharf08@gmail.com>
2022-08-11 08:54:30 +0200

    Makes main.py run as FreeCAD macro

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import os, sys, inspect
-from qtpy.QtWidgets import QApplication
+import sys
 
+from qtpy.QtWidgets import QApplication, QMainWindow
 from nodeeditor.node_editor_window import NodeEditorWindow
+import FreeCADGui
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
-    wnd = NodeEditorWindow()
-    wnd.nodeeditor.addNodes()
-    module_path = os.path.dirname( inspect.getfile(wnd.__class__) )
-
-    sys.exit(app.exec_())
+    if hasattr(FreeCADGui, "getMainWindow"):
+        container = QMainWindow(parent=FreeCADGui.getMainWindow())
+        ne_wnd = NodeEditorWindow()
+        ne_wnd.nodeeditor.addNodes()
+        container.setCentralWidget(ne_wnd)
+        container.show()
+    else:
+        app = QApplication(sys.argv)
+        wnd = NodeEditorWindow()
+        wnd.nodeeditor.addNodes()
+        sys.exit(app.exec_())
