71db46301420a9b290c1129284811c5d85fb998c
j8sr0230 <ronny.scharf08@gmail.com>
2022-08-11 14:25:22 +0200

    Refactors fc_nodes_drag_listbox.py

--- fc_nodes_drag_listbox.py
+++ fc_nodes_drag_listbox.py
@@ -1,68 +1,59 @@
 from qtpy.QtGui import QPixmap, QIcon, QDrag
 from qtpy.QtCore import QSize, Qt, QByteArray, QDataStream, QMimeData, QIODevice, QPoint
 from qtpy.QtWidgets import QListWidget, QAbstractItemView, QListWidgetItem
+from nodeeditor.utils import dumpException
 
 from fc_nodes_conf import FC_NODES, get_class_from_opcode, LISTBOX_MIMETYPE
-from nodeeditor.utils import dumpException
 
 
 class QDMDragListbox(QListWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
-        # init
+    def init_ui(self):
         self.setIconSize(QSize(32, 32))
         self.setSelectionMode(QAbstractItemView.SingleSelection)
         self.setDragEnabled(True)
+        self.add_my_items()
 
-        self.addMyItems()
-
-
-    def addMyItems(self):
+    def add_my_items(self):
         keys = list(FC_NODES.keys())
         keys.sort()
         for key in keys:
             node = get_class_from_opcode(key)
-            self.addMyItem(node.op_title, node.icon, node.op_code)
+            self.add_my_item(node.op_title, node.icon, node.op_code)
 
-
-    def addMyItem(self, name, icon=None, op_code=0):
-        item = QListWidgetItem(name, self) # can be (icon, text, parent, <int>type)
+    def add_my_item(self, name, icon=None, op_code=0):
+        item = QListWidgetItem(name, self)  # Can be (icon, text, parent, <int>type)
         pixmap = QPixmap(icon if icon is not None else ".")
         item.setIcon(QIcon(pixmap))
         item.setSizeHint(QSize(32, 32))
-
         item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled)
 
         # setup data
         item.setData(Qt.UserRole, pixmap)
         item.setData(Qt.UserRole + 1, op_code)
 
-
     def startDrag(self, *args, **kwargs):
         try:
             item = self.currentItem()
             op_code = item.data(Qt.UserRole + 1)
-
             pixmap = QPixmap(item.data(Qt.UserRole))
 
-
-            itemData = QByteArray()
-            dataStream = QDataStream(itemData, QIODevice.WriteOnly)
-            dataStream << pixmap
-            dataStream.writeInt32(op_code)
-            dataStream.writeQString(item.text())
-
-            mimeData = QMimeData()
-            mimeData.setData(LISTBOX_MIMETYPE, itemData)
+            item_data = QByteArray()
+            data_stream = QDataStream(item_data, QIODevice.WriteOnly)
+            data_stream << pixmap
+            data_stream.writeInt32(op_code)
+            data_stream.writeQString(item.text())
+            mime_data = QMimeData()
+            mime_data.setData(LISTBOX_MIMETYPE, item_data)
 
             drag = QDrag(self)
-            drag.setMimeData(mimeData)
+            drag.setMimeData(mime_data)
             drag.setHotSpot(QPoint(pixmap.width() // 2, pixmap.height() // 2))
             drag.setPixmap(pixmap)
-
             drag.exec_(Qt.MoveAction)
 
-        except Exception as e: dumpException(e)
+        except Exception as e:
+            dumpException(e)
