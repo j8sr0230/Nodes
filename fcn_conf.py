18bc226974c6c36e040ce60534a00e8f3062cb6f
flachyjoe <flachyjoe@gmail.com>
2022-09-04 14:46:28 +0200

    improve nodes import
    import nodes from filename
    - Faulty node code doesn't stop the execution
    - First step to load node at runtime
    - First step to reload node on code change

--- fcn_conf.py
+++ fcn_conf.py
@@ -39,5 +39,30 @@ def get_class_from_opcode(op_code):
         raise OpCodeNotRegistered("OpCode '%d' is not registered" % op_code)
     return FC_NODES[op_code]
 
+# Import nodes
+import importlib.util
+import sys
+from os.path import dirname, basename, isfile, join
+from glob import glob
 
-from nodes import *  # Import all nodes and register them
+import fcn_locator as locator
+
+def add_node_from_file(file_path):
+    module_name = basename(file_path)[:-3]
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    try:
+        spec.loader.exec_module(module)
+        print(f'New node {module_name}')
+    except Exception as e:
+        print(f'Unable to add node from "{file_path}": {e}')
+
+NODES_DIR = [locator.NODES_PATH,] #TODO add user's nodes directory from workbench pref
+
+for directory in NODES_DIR:
+    files = [f for f in glob(join(directory, "*.py")) if isfile(f)]
+    for file_path in files:
+        add_node_from_file(file_path)
+
+# TODO allow to add node dynamicaly, needs GUI refresh
