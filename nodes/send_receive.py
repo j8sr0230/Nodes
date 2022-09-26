6cacfddf91e2f5d495a2019f7a4d1607415153a0
flachyjoe <flachyjoe@gmail.com>
2022-09-26 22:56:44 +0200

    Merge sender.py and receiver.py

--- nodes/send_receive.py
+++ nodes/send_receive.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###################################################################################
 #
-#  sender.py
+#  send_receive.py
 #
 #  Copyright (c) 2022 Ronny Scharf-Wildenhain <ronny.scharf08@gmail.com>
 #
@@ -78,3 +78,56 @@ class Sender(FCNNode):
         self.data = input_data
         self.push_data_signal.send(input_data)
         return [[signal_id], input_data]
+
+
+@register_node
+class Receiver(FCNNode):
+    data: list
+    icon: str = icon("fcn_default.png")
+    op_title: str = "Receiver"
+    content_label_objname: str = "fcn_node_bg"
+
+    def __init__(self, scene):
+        self.data: list = []
+        self.signal_id: str = ""
+        self.push_data_signal: signal = None
+        self.pull_data_signal: signal = signal("pull")
+
+        super().__init__(scene=scene,
+                         inputs_init_list=[(3, "Id", 1, "1", False, ('str', ))],
+                         outputs_init_list=[(3, "Id", 0, "1", True, ('str', )), (6, "Out", 0, 0, True, ('*', ))],
+                         width=150)
+
+    def collapse_node(self, collapse: bool = False):
+        super().collapse_node(collapse)
+        if collapse is True:
+            input_str = self.content.input_widgets[0].text()
+            if input_str.isdigit():
+                self.title = 'Receiver at <%d>' % int(self.content.input_widgets[0].text())
+            else:
+                self.title = 'Receiver at ' + self.content.input_widgets[0].text()
+        else:
+            self.title = self.default_title
+
+    def on_push(self, data):
+        try:
+            self.data = data
+            self.markDirty()
+            self.eval()
+        except Exception as e:
+            print(e, data)
+
+    def eval_operation(self, sockets_input_data: list) -> list:
+        signal_id = sockets_input_data[0][0]
+
+        if self.signal_id != signal_id:
+            # New receiver signale id
+            if hasattr(self.push_data_signal, "disconnect"):
+                self.push_data_signal.disconnect(self.on_push)
+                self.push_data_signal = None
+            self.push_data_signal = signal(signal_id)
+            self.signal_id = signal_id
+            self.push_data_signal.connect(self.on_push)
+            self.pull_data_signal.send(self)
+
+        return [[signal_id], self.data]
