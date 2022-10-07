f212c10e3be93358cd56094f22adf3085b7c31a3
j8sr0230 <ronny.scharf08@gmail.com>
2022-10-07 19:59:25 +0200

    Adds pivy preview to MakePoint node

--- nodes/make_point.py
+++ nodes/make_point.py
@@ -22,8 +22,10 @@
 #
 #
 ###################################################################################
-from FreeCAD import Vector
-import Part
+# from FreeCAD import Vector
+# import Part
+import FreeCADGui as Gui
+import pivy.coin as coin
 
 from fcn_conf import register_node
 from fcn_base_node import FCNNode
@@ -40,19 +42,55 @@ class MakePoint(FCNNode):
     content_label_objname: str = "fcn_node_bg"
 
     def __init__(self, scene):
+        self.sg_node = None
+
         super().__init__(scene=scene,
                          inputs_init_list=[(1, "Pos", 0, 0, True, ("int", "float"))],
                          outputs_init_list=[(5, "Shp", 0, 0, True, ("Shape", ))],
                          width=150)
 
+    def remove(self):
+        super().remove()
+
+        if hasattr(Gui, "ActiveDocument"):
+            view = Gui.ActiveDocument.ActiveView
+            sg = view.getSceneGraph()
+            if self.sg_node is not None:
+                sg.removeChild(self.sg_node)
+
     def eval_operation(self, sockets_input_data: list) -> list:
         position_list: list = sockets_input_data[0]
 
-        vertex_list = []
+        # vertex_list = []
         vector_pos_list = flatten_to_vectors(position_list)
 
-        for vec in vector_pos_list:
-            vertex = Part.Point(Vector(vec)).toShape()
-            vertex_list.append(vertex)
+        # for vec in vector_pos_list:
+        #     vertex = Part.Point(Vector(vec)).toShape()
+        #     vertex_list.append(vertex)
+        #
+        # return [vertex_list]
 
-        return [vertex_list]
+        if hasattr(Gui, "ActiveDocument"):
+            view = Gui.ActiveDocument.ActiveView
+            sg = view.getSceneGraph()
+
+            marker = coin.SoMarkerSet()
+            marker.markerIndex = coin.SoMarkerSet.CIRCLE_FILLED_9_9
+
+            data = coin.SoCoordinate3()
+            data.point.setValues(0, len(vector_pos_list), vector_pos_list)
+
+            color = coin.SoMaterial()
+            color.diffuseColor = (1., 0, 0)
+
+            if self.sg_node is not None:
+                sg.removeChild(self.sg_node)
+
+            self.sg_node = coin.SoSeparator()
+            self.sg_node.addChild(color)
+            self.sg_node.addChild(data)
+            self.sg_node.addChild(marker)
+
+            sg.addChild(self.sg_node)
+
+        return [vector_pos_list]
