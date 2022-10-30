2ae1f8df49b87ecf82184d8ccb2b97197a842995
j8sr0230 <ronny.scharf08@gmail.com>
2022-10-30 15:07:06 +0100

    Merges fcn_base_node.py with fcn_graphics_node.py

--- fcn_graphics_node.py
+++ fcn_graphics_node.py
@@ -58,15 +58,33 @@ class QGraphicsNode(QGraphicsItem):
     _was_moved: bool
     _last_selected_state: bool
     _title: str
-
     width: int
     height: int
-    collapsed_height: int
-    default_height: int
     edge_roundness: int
     edge_padding: int
+    title_height: int
     title_horizontal_padding: int
     title_vertical_padding: int
+    _title_color: QColor
+    _title_font: QFont
+
+    _color: QColor
+    _color_selected: QColor
+    _color_hovered: QColor
+
+    _pen_default: QPen
+    _pen_selected: QPen
+    _pen_hovered: QPen
+
+    _brush_title: QBrush
+    _brush_background: QBrush
+
+    title_item: QGraphicsTextItem
+    grContent: QGraphicsProxyWidget
+
+    collapsed_height: int
+    default_height: int
+
     icons: QImage
     main_icon: QImage
 
@@ -119,20 +137,23 @@ class QGraphicsNode(QGraphicsItem):
         self.setAcceptHoverEvents(True)
 
         # init title
-        self.initTitle()
+        self.init_title()
         self.title = self.node.title
 
-        self.initContent()
+        self.init_content()
 
     def init_sizes(self):
         """Set up internal attributes like `width`, `height`, etc."""
         self.width = 180
         self.height = 240
-        self.edge_roundness = 10.0
-        self.edge_padding = 10.0
+        self.edge_roundness = 10
+        self.edge_padding = 10
         self.title_height = 24
-        self.title_horizontal_padding = 4.0
-        self.title_vertical_padding = 4.0
+        self.title_horizontal_padding = 4
+        self.title_vertical_padding = 4
+
+        self.default_height: int = self.height
+        self.collapsed_height: int = 50
 
     def init_assets(self):
         """Initialize ``QObjects`` like ``QColor``, ``QPen`` and ``QBrush``"""
@@ -144,20 +165,22 @@ class QGraphicsNode(QGraphicsItem):
         self._color_hovered = QColor("#FF37A6FF")
 
         self._pen_default = QPen(self._color)
-        self._pen_default.setWidthF(2.0)
-        self._pen_selected = QPen(self._color_selected)
-        self._pen_selected.setWidthF(2.0)
+        self._pen_selected: QPen
         self._pen_hovered = QPen(self._color_hovered)
         self._pen_hovered.setWidthF(3.0)
 
         self._brush_title = QBrush(QColor("#FF313131"))
         self._brush_background = QBrush(QColor("#E3212121"))
 
-    def onSelected(self):
+        path: str = locator.icon("fcn_status_icon.png")
+        self.icons: QImage = QImage(path)
+        self.main_icon: QImage = QImage(self.node.icon)
+
+    def on_selected(self):
         """Our event handling when the node was selected"""
         self.node.scene.grScene.itemSelected.emit()
 
-    def doSelect(self, new_state=True):
+    def do_select(self, new_state=True):
         """Safe version of selecting the `Graphics Node`. Takes care about the selection state flag used internally
 
         :param new_state: ``True`` to select, ``False`` to deselect
@@ -165,7 +188,8 @@ class QGraphicsNode(QGraphicsItem):
         """
         self.setSelected(new_state)
         self._last_selected_state = new_state
-        if new_state: self.onSelected()
+        if new_state:
+            self.onSelected()
 
     def mouseMoveEvent(self, event):
         """Overridden event to detect that we moved with this `Node`"""
@@ -196,7 +220,8 @@ class QGraphicsNode(QGraphicsItem):
             return
 
         # handle when grNode was clicked on
-        if self._last_selected_state != self.isSelected() or self.node.scene._last_selected_items != self.node.scene.getSelectedItems():
+        if self._last_selected_state != self.isSelected() or \
+                self.node.scene._last_selected_items != self.node.scene.getSelectedItems():
             self.node.scene.resetLastSelectedStates()
             self._last_selected_state = self.isSelected()
             self.onSelected()
@@ -215,18 +240,11 @@ class QGraphicsNode(QGraphicsItem):
         self.hovered = False
         self.update()
 
-
     def boundingRect(self) -> QRectF:
         """Defining Qt' bounding rectangle"""
-        return QRectF(
-            0,
-            0,
-            self.width,
-            self.height
-        ).normalized()
+        return QRectF(0, 0, self.width, self.height).normalized()
 
-
-    def initTitle(self):
+    def init_title(self):
         """Set up the title Graphics representation: font, color, position, etc."""
         self.title_item = QGraphicsTextItem(self)
         self.title_item.node = self.node
@@ -238,41 +256,48 @@ class QGraphicsNode(QGraphicsItem):
             - 2 * self.title_horizontal_padding
         )
 
-    def initContent(self):
+    def init_content(self):
         """Set up the `grContent` - ``QGraphicsProxyWidget`` to have a container for `Graphics Content`"""
         if self.content is not None:
             self.content.setGeometry(self.edge_padding, self.title_height + self.edge_padding,
-                                 self.width - 2 * self.edge_padding, self.height - 2 * self.edge_padding - self.title_height)
+                                     self.width - 2 * self.edge_padding, self.height - 2 * self.edge_padding -
+                                     self.title_height)
 
         # get the QGraphicsProxyWidget when inserted into the grScene
         self.grContent = self.node.scene.grScene.addWidget(self.content)
         self.grContent.node = self.node
         self.grContent.setParentItem(self)
 
-
-    def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
-        """Painting the rounded rectanglar `Node`"""
+    def paint(self, painter, option, widget=None, **kwargs):
+        """Painting the rounded rectangular `Node`
+        :param widget:
+        :type widget:
+        :param option:
+        :type option:
+        :type painter: object:
+        """
         # title
         path_title = QPainterPath()
         path_title.setFillRule(Qt.WindingFill)
         path_title.addRoundedRect(0, 0, self.width, self.title_height, self.edge_roundness, self.edge_roundness)
         path_title.addRect(0, self.title_height - self.edge_roundness, self.edge_roundness, self.edge_roundness)
-        path_title.addRect(self.width - self.edge_roundness, self.title_height - self.edge_roundness, self.edge_roundness, self.edge_roundness)
+        path_title.addRect(self.width - self.edge_roundness, self.title_height - self.edge_roundness,
+                           self.edge_roundness, self.edge_roundness)
         painter.setPen(Qt.NoPen)
         painter.setBrush(self._brush_title)
         painter.drawPath(path_title.simplified())
 
-
         # content
         path_content = QPainterPath()
         path_content.setFillRule(Qt.WindingFill)
-        path_content.addRoundedRect(0, self.title_height, self.width, self.height - self.title_height, self.edge_roundness, self.edge_roundness)
+        path_content.addRoundedRect(0, self.title_height, self.width, self.height - self.title_height,
+                                    self.edge_roundness, self.edge_roundness)
         path_content.addRect(0, self.title_height, self.edge_roundness, self.edge_roundness)
-        path_content.addRect(self.width - self.edge_roundness, self.title_height, self.edge_roundness, self.edge_roundness)
+        path_content.addRect(self.width - self.edge_roundness, self.title_height, self.edge_roundness,
+                             self.edge_roundness)
         painter.setPen(Qt.NoPen)
         painter.setBrush(self._brush_background)
         painter.drawPath(path_content.simplified())
-
 
         # outline
         path_outline = QPainterPath()
@@ -286,3 +311,10 @@ class QGraphicsNode(QGraphicsItem):
         else:
             painter.setPen(self._pen_default if not self.isSelected() else self._pen_selected)
             painter.drawPath(path_outline.simplified())
+
+        painter.drawImage(QRectF(-12, -12, 24, 24), self.main_icon, QRectF(self.main_icon.rect()))
+        status_icon_placement = QRectF(self.width - 12, -12, 24.0, 24.0)
+        if self.node.isDirty():
+            painter.drawImage(status_icon_placement, self.icons, QRectF(0, 0, 24, 24))
+        if self.node.isInvalid():
+            painter.drawImage(status_icon_placement, self.icons, QRectF(48, 0, 24, 24))
