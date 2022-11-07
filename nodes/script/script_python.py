93948dd0eb19bb7b45b4c434aa999a4750e8c84b
j8sr0230 <ronny.scharf08@gmail.com>
2022-11-07 06:46:41 +0100

    Finishes PyScript node

--- nodes/script/script_python.py
+++ nodes/script/script_python.py
@@ -22,6 +22,8 @@
 #
 #
 ###################################################################################
+from collections import OrderedDict
+
 from qtpy.QtWidgets import QSizePolicy, QDialog, QVBoxLayout, QHBoxLayout, QPushButton, QWidget
 from nodeeditor.node_content_widget import QTextEdit
 
@@ -31,23 +33,26 @@ from nodes_locator import icon
 
 
 class CodeEditorDialog(QDialog):
+
+    node: FCNNodeModel
     layout: QVBoxLayout
     code_input_widget: QTextEdit
     ok_button: QPushButton
     cancel_button: QPushButton
-    node: FCNNodeModel
 
     def __init__(self, node: FCNNodeModel = None, parent: QWidget = None):
         super().__init__(parent)
-        self.init_ui()
         self.node = node
+        self.init_ui()
 
     def init_ui(self):
         self.layout: QVBoxLayout = QVBoxLayout()
         self.setLayout(self.layout)
+        self.setWindowTitle("Text Editor")
 
         # Code input widget
-        self.code_input_widget: QTextEdit = QTextEdit("output_data = input_data\nprint(output_data)", self)
+        self.code_input_widget: QTextEdit = QTextEdit(self)
+        self.code_input_widget.setPlainText(self.node.code_string)
         self.layout.addWidget(self.code_input_widget)
 
         # Button row
@@ -58,40 +63,57 @@ class CodeEditorDialog(QDialog):
         self.layout.addWidget(button_widget)
 
         self.ok_button: QPushButton = QPushButton("Ok")
-        self.ok_button.clicked.connect(self.set_code)
+        self.ok_button.clicked.connect(self.set_code_string)
+        button_widget_layout.addWidget(self.ok_button)
 
         self.cancel_button: QPushButton = QPushButton("Cancel")
         self.cancel_button.clicked.connect(self.hide)
-
-        button_widget_layout.addWidget(self.ok_button)
         button_widget_layout.addWidget(self.cancel_button)
 
-    def set_code(self):
-        self.node.code = self.code_input_widget.toPlainText()
+    def set_code_string(self):
+        self.node.code_string = self.code_input_widget.toPlainText()
+        self.node.markDirty()
         self.node.eval()
         self.hide()
 
 
 @register_node
-class PythonNode(FCNNodeModel):
+class PyScript(FCNNodeModel):
 
     icon: str = icon("nodes_python_logo.png")
-    op_title: str = "Python"
+    op_title: str = "Py Script"
     op_category: str = "Script"
     content_label_objname: str = "fcn_node_bg"
 
-    code_dialog: QDialog
-    code: str
+    code_editor_dialog: QDialog
+    code_string: str
 
     def __init__(self, scene):
-        self.code: srt = "output_data = input_data\nprint(output_data)"
+        self.code_string: str = "output_data = input_data\nprint(output_data)"
         super().__init__(scene=scene, inputs_init_list=[("In", True)], outputs_init_list=[("Out", True)])
 
+        self.grNode.resize(90, 70)
+        for socket in self.inputs + self.outputs:
+            socket.setSocketPosition()
+
     def onDoubleClicked(self, event):
-        self.code_dialog = CodeEditorDialog(node=self, parent=self.scene.getView())
-        self.code_dialog.show()
+        self.code_editor_dialog = CodeEditorDialog(node=self, parent=self.scene.getView())
+        self.code_editor_dialog.show()
 
     def eval_operation(self, sockets_input_data: list) -> list:
         namespace = {'input_data': sockets_input_data[0], 'output_data': None}
-        exec(self.code, namespace)
+        exec(self.code_string, namespace)
         return [namespace['output_data']]
+
+    def serialize(self) -> OrderedDict:
+        res = super().serialize()
+        res['code_string'] = self.code_string
+        return res
+
+    def deserialize(self, data: dict, hashmap=None, restore_id: bool = True, *args, **kwargs) -> bool:
+        if hashmap is None:
+            hashmap = {}
+        res = super().deserialize(data, hashmap, restore_id)
+
+        self.code_string = data['code_string']
+        return res
