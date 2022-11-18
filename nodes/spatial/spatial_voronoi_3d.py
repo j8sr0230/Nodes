5c7c67ee7fae3e2ea6ad7fd445c2615bec17b127
j8sr0230 <ronny.scharf08@gmail.com>
2022-11-18 17:54:46 +0100

    Adds voronoi3d_regions method from Sverchok

--- nodes/spatial/spatial_voronoi_3d.py
+++ nodes/spatial/spatial_voronoi_3d.py
@@ -25,6 +25,7 @@
 import numpy as np
 from scipy.spatial import Voronoi
 from scipy import rand
+from collections import defaultdict
 
 from FreeCAD import Vector
 import Part
@@ -57,14 +58,91 @@ class Voronoi3D(FCNNodeModel):
             socket.setSocketPosition()
 
     @staticmethod
-    def make_voronoi(object_zip: tuple) -> Part.Shape:
+    def voronoi3d_regions(points, closed_only=True, recalc_normals=True, do_clip=False, clipping=1.0):
+        diagram = Voronoi(points)
+        faces_per_point = defaultdict(list)
+        nsites = len(diagram.point_region)
+        nridges = len(diagram.ridge_points)
+        open_sites = set()
+        for ridge_idx in range(nridges):
+            site_idx_1, site_idx_2 = diagram.ridge_points[ridge_idx]
+            face = diagram.ridge_vertices[ridge_idx]
+            if -1 in face:
+                open_sites.add(site_idx_1)
+                open_sites.add(site_idx_2)
+                continue
+            faces_per_point[site_idx_1].append(face)
+            faces_per_point[site_idx_2].append(face)
+
+        print(diagram.vertices)
+
+        # new_verts = []
+        # new_edges = []
+        # new_faces = []
+        #
+        # for site_idx in sorted(faces_per_point.keys()):
+        #     if closed_only and site_idx in open_sites:
+        #         continue
+        #     done_verts = dict()
+        #     bm = bmesh.new()
+        #     add_vert = bm.verts.new
+        #     add_face = bm.faces.new
+        #     for face in faces_per_point[site_idx]:
+        #         face_bm_verts = []
+        #         for vertex_idx in face:
+        #             if vertex_idx not in done_verts:
+        #                 bm_vert = add_vert(diagram.vertices[vertex_idx])
+        #                 done_verts[vertex_idx] = bm_vert
+        #             else:
+        #                 bm_vert = done_verts[vertex_idx]
+        #             face_bm_verts.append(bm_vert)
+        #         add_face(face_bm_verts)
+        #     bm.verts.index_update()
+        #     bm.verts.ensure_lookup_table()
+        #     bm.faces.index_update()
+        #     bm.edges.index_update()
+        #
+        #     if closed_only and any(v.is_boundary for v in bm.verts):
+        #         bm.free()
+        #         continue
+        #
+        #     if recalc_normals:
+        #         bm.normal_update()
+        #         bmesh.ops.recalc_face_normals(bm, faces=bm.faces[:])
+        #
+        #     region_verts, region_edges, region_faces = pydata_from_bmesh(bm)
+        #     bm.free()
+        #     new_verts.append(region_verts)
+        #     new_edges.append(region_edges)
+        #     new_faces.append(region_faces)
+        #
+        # if do_clip:
+        #     verts_n, edges_n, faces_n = [], [], []
+        #     bounds = calc_bounds(points, clipping)
+        #     for verts_i, edges_i, faces_i in zip(new_verts, new_edges, new_faces):
+        #         bm = bmesh_from_pydata(verts_i, edges_i, faces_i)
+        #         bmesh_clip(bm, bounds, fill=True)
+        #         bm.normal_update()
+        #         bmesh.ops.recalc_face_normals(bm, faces=bm.faces[:])
+        #         verts_i, edges_i, faces_i = pydata_from_bmesh(bm)
+        #         bm.free()
+        #         verts_n.append(verts_i)
+        #         edges_n.append(edges_i)
+        #         faces_n.append(faces_i)
+        #     new_verts, new_edges, new_faces = verts_n, edges_n, faces_n
+        #
+        # return new_verts, new_edges, new_faces
+
+    def make_voronoi(self, object_zip: tuple) -> Part.Shape:
         face: Part.Face = object_zip[0]
         points: list = object_zip[1]
 
-        base_pts = rand(20, 3) * 100
-        vor = Voronoi(points=base_pts)
-        #vor: Voronoi = Voronoi(np.array(points), qhull_options="QJ")
-        print(vor.vertices)
+        self.voronoi3d_regions(np.array(points))
+
+        # base_pts = rand(20, 3) * 100
+        # vor = Voronoi(points=base_pts)
+        # vor: Voronoi = Voronoi(np.array(points), qhull_options="QJ")
+        # print(vor.vertices)
 
         # Filter region for valid vectors
         # vor_points = vor.vertices
