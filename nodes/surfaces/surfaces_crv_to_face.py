87293a904da749141fcce599cb23448c6c419e86
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-11-21 11:44:55 +0100

    Update surfaces_crv_to_face.py
    icon("Surface_BSplineSurface.svg")

--- nodes/surfaces/surfaces_crv_to_face.py
+++ nodes/surfaces/surfaces_crv_to_face.py
@@ -37,7 +37,7 @@ from nodes_locator import icon
 @register_node
 class CrvToFace(FCNNodeModel):
 
-    icon: str = icon("nodes_default.png")
+    icon: str = icon("Surface_BSplineSurface.svg")
     op_title: str = "Crv to Face"
     op_category: str = "Surfaces"
     content_label_objname: str = "fcn_node_bg"
