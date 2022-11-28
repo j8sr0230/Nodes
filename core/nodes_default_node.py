3e826341e1320ca2fc693e00e237b9d1037d5113
j8sr0230 <ronny.scharf08@gmail.com>
2022-11-28 15:33:11 +0100

    Adds UV on Srf node

--- core/nodes_default_node.py
+++ core/nodes_default_node.py
@@ -295,11 +295,11 @@ class FCNNodeModel(Node):
         try:
             # Run new evaluation and return the desired output socket (index)
             return self.eval_primer()[index]
-        # except (ValueError, TypeError, SyntaxError, NameError, ZeroDivisionError, IndexError, AttributeError,
-        #         OCCError) as e:
-        #     self.markInvalid()
-        #     self.grNode.setToolTip(str(e))
-        #     self.markDescendantsDirty()
+        except (ValueError, TypeError, SyntaxError, NameError, ZeroDivisionError, IndexError, AttributeError,
+                OCCError) as e:
+            self.markInvalid()
+            self.grNode.setToolTip(str(e))
+            self.markDescendantsDirty()
         except Exception as e:
             self.markInvalid()
             self.grNode.setToolTip(str(e))
