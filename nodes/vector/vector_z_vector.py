8bd81ad4feb3b326ee4f8e739c61d7c092b29a2e
j8sr0230 <ronny.scharf08@gmail.com>
2022-11-28 08:14:11 +0100

    Adds predefined Vectors for x, y and z

---
+++ nodes/vector/vector_z_vector.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#  vector_z_vector.py
+#
+#  Copyright (c) 2022 Ronny Scharf-Wildenhain <ronny.scharf08@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+###################################################################################
+from FreeCAD import Vector
+import awkward as ak
+
+from core.nodes_default_node import FCNNodeModel
+from core.nodes_conf import register_node
+from core.nodes_utils import flatten, map_objects
+
+from nodes_locator import icon
+
+
+@register_node
+class ZVector(FCNNodeModel):
+
+    icon: str = icon("nodes_default.png")
+    op_title: str = "Z Vector"
+    op_category: str = "Vector"
+    content_label_objname: str = "fcn_node_bg"
+
+    def __init__(self, scene):
+        super().__init__(scene=scene,
+                         inputs_init_list=[],
+                         outputs_init_list=[("Out", True)])
+
+        self.grNode.resize(90, 70)
+        for socket in self.inputs + self.outputs:
+            socket.setSocketPosition()
+
+    def eval_operation(self, sockets_input_data: list) -> list:
+        return [[Vector(0, 0, 1)]]
