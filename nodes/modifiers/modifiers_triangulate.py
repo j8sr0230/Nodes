f0d7ce57742c1d499e44d1741fda31b17882bb7d
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-11-29 17:45:38 +0100

    Update modifiers_triangulate.py

--- nodes/modifiers/modifiers_triangulate.py
+++ nodes/modifiers/modifiers_triangulate.py
@@ -39,7 +39,7 @@ from nodes_locator import icon
 @register_node
 class Triangulate(FCNNodeModel):
 
-    icon: str = icon("nodes_default.png")
+    icon: str = icon("nodes_triangulate.svg")
     op_title: str = "Triangulate"
     op_category: str = "Modifiers"
     content_label_objname: str = "fcn_node_bg"
