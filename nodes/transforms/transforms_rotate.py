ebcdd575be568bf6c34e51f5ba7346f97493eaa2
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-11-30 18:19:40 +0100

    Update transforms_rotate.py

--- nodes/transforms/transforms_rotate.py
+++ nodes/transforms/transforms_rotate.py
@@ -37,7 +37,7 @@ from nodes_locator import icon
 @register_node
 class Rotate(FCNNodeModel):
 
-    icon: str = icon("nodes_default.png")
+    icon: str = icon("nodes_rotate.svg")
     op_title: str = "Rotate"
     op_category: str = "Transforms"
     content_label_objname: str = "fcn_node_bg"
