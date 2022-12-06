f1ba63f8012f2bf5e96cfa9e3e6fa9b6d999d5a7
j8sr0230 <ronny.scharf08@gmail.com>
2022-12-06 13:45:11 +0100

    Refactors number nodes

--- nodes/number/number_add.py
+++ nodes/number/number_add.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###################################################################################
 #
-#  number_number_range.py
+#  number_add.py
 #
 #  Copyright (c) 2022 Ronny Scharf-Wildenhain <ronny.scharf08@gmail.com>
 #
@@ -22,48 +22,33 @@
 #
 #
 ###################################################################################
-import numpy as np
+import awkward as ak
 
 from core.nodes_conf import register_node
 from core.nodes_default_node import FCNNodeModel
-from core.nodes_utils import map_objects, broadcast_data_tree
 
 from nodes_locator import icon
 
 
 @register_node
-class NumberRange(FCNNodeModel):
+class Add(FCNNodeModel):
 
-    icon: str = icon("nodes_number_range.svg")
-    op_title: str = "Number Range"
+    icon: str = icon("nodes_math.svg")
+    op_title: str = "Add"
     op_category: str = "Number"
     content_label_objname: str = "fcn_node_bg"
 
     def __init__(self, scene):
         super().__init__(scene=scene,
-                         inputs_init_list=[("Start", True), ("Stop", True), ("Step", True)],
+                         inputs_init_list=[("A", True), ("B", True)],
                          outputs_init_list=[("Out", True)])
-
-        self.grNode.resize(130, 100)
-        for socket in self.inputs + self.outputs:
-            socket.setSocketPosition()
-
-    @staticmethod
-    def make_range(parameter_zip: tuple) -> list:
-        start: float = parameter_zip[0]
-        stop: float = parameter_zip[1]
-        step: float = parameter_zip[2]
-
-        return list(np.arange(start, stop, step))
 
     def eval_operation(self, sockets_input_data: list) -> list:
         # Get socket inputs
-        start_input = sockets_input_data[0] if len(sockets_input_data[0]) > 0 else [0.0]
-        stop_input = sockets_input_data[1] if len(sockets_input_data[1]) > 0 else [10.0]
-        step_input = sockets_input_data[2] if len(sockets_input_data[2]) > 0 else [1.0]
+        a_input = ak.Array(sockets_input_data[0] if len(sockets_input_data[0]) > 0 else [0])
+        b_input = ak.Array(sockets_input_data[1] if len(sockets_input_data[1]) > 0 else [0])
 
         # Broadcast and calculate result
-        data_tree: list = list(broadcast_data_tree(start_input, stop_input, step_input))
-        ranges: list = list(map_objects(data_tree, tuple, self.make_range))
+        res = a_input + b_input
 
-        return [ranges]
+        return [res.tolist()]
