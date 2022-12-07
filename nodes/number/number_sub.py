a3fbd631b04e1039b2f4da41c934e2672f5b0751
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-12-07 10:06:35 +0100

    Update number_sub.py

--- nodes/number/number_sub.py
+++ nodes/number/number_sub.py
@@ -33,7 +33,7 @@ from nodes_locator import icon
 @register_node
 class Sub(FCNNodeModel):
 
-    icon: str = icon("nodes_math.svg")
+    icon: str = icon("nodes_sub.svg")
     op_title: str = "Sub"
     op_category: str = "Number"
     content_label_objname: str = "fcn_node_bg"
