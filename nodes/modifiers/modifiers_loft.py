c9faa905dbcae310416674c46d8afe3da31a1170
j8sr0230 <ronny.scharf08@gmail.com>
2022-12-09 06:30:55 +0100

    Implements Loft node

--- nodes/modifiers/modifiers_loft.py
+++ nodes/modifiers/modifiers_loft.py
@@ -27,42 +27,48 @@ import Part
 
 from core.nodes_conf import register_node
 from core.nodes_default_node import FCNNodeModel
-from core.nodes_utils import map_objects, broadcast_data_tree
+from core.nodes_utils import map_objects, map_last_level, broadcast_data_tree, ListWrapper
 
 from nodes_locator import icon
 
 
 @register_node
-class Extrude(FCNNodeModel):
+class Loft(FCNNodeModel):
 
-    icon: str = icon("nodes_extrude.svg")
-    op_title: str = "Extrude"
+    icon: str = icon("nodes_default.png")
+    op_title: str = "Loft"
     op_category: str = "Modifiers"
     content_label_objname: str = "fcn_node_bg"
 
     def __init__(self, scene):
         super().__init__(scene=scene,
-                         inputs_init_list=[("Shape", True), ("Direction", True)],
+                         inputs_init_list=[("Shape", True), ("Solid", True), ("Ruled", True)],
                          outputs_init_list=[("Shape", True)])
 
-        self.grNode.resize(120, 80)
+        self.grNode.resize(100, 100)
         for socket in self.inputs + self.outputs:
             socket.setSocketPosition()
 
     @staticmethod
-    def make_extrusion(parameter_zip: tuple) -> Part.Shape:
-        shape: Part.Shape = parameter_zip[0]
-        direction: Vector = parameter_zip[1]
+    def make_loft(parameter_zip: tuple) -> Part.Shape:
+        sections: list = parameter_zip[0].wrapped_data if hasattr(parameter_zip[0], 'wrapped_data') else []
+        solid: bool = parameter_zip[1]
+        ruled: bool = parameter_zip[1]
 
-        return shape.extrude(direction)
+        return Part.makeLoft(sections, solid, ruled)
 
     def eval_operation(self, sockets_input_data: list) -> list:
         # Get socket inputs
-        shape_input: list = sockets_input_data[0]
-        direction_input: list = sockets_input_data[1] if len(sockets_input_data[1]) > 0 else [Vector(0, 0, 1)]
+        shape_input: list = [sockets_input_data[0]]
+        solid_input: list = sockets_input_data[1] if len(sockets_input_data[1]) > 0 else [True]
+        ruled_input: list = sockets_input_data[2] if len(sockets_input_data[2]) > 0 else [False]
+
+        # Needed, to treat list as atomic object during array broadcasting
+        wrapped_shape_input: list = list(map_last_level(shape_input, Part.Shape | Part.BSplineCurve | Part.Arc,
+                                                        ListWrapper))
 
         # Broadcast and calculate result
-        data_tree: list = list(broadcast_data_tree(shape_input, direction_input))
-        shapes: list = list(map_objects(data_tree, tuple, self.make_extrusion))
+        data_tree: list = list(broadcast_data_tree(wrapped_shape_input, solid_input, ruled_input))
+        lofts: list = list(map_objects(data_tree, tuple, self.make_loft))
 
-        return [shapes]
+        return [lofts]
