b33fd16a4fe28b268c6b2f0eaea478f35c180480
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-12-12 10:15:16 +0100

    Update generators_solid_cylinder.py

--- nodes/generators/generators_solid_cylinder.py
+++ nodes/generators/generators_solid_cylinder.py
@@ -35,7 +35,7 @@ from nodes_locator import icon
 @register_node
 class SolidCylinder(FCNNodeModel):
 
-    icon: str = icon("nodes_default.png")
+    icon: str = icon("nodes_cylinder.svg")
     op_title: str = "Cylinder"
     op_category: str = "Generators"
     content_label_objname: str = "fcn_node_bg"
