735b5e8c1e4aaeaf1b3159d6fdd869c1ca15b2c7
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2022-12-12 10:13:52 +0100

    Update curves_helix.py

--- nodes/curves/curves_helix.py
+++ nodes/curves/curves_helix.py
@@ -35,7 +35,7 @@ from nodes_locator import icon
 @register_node
 class Helix(FCNNodeModel):
 
-    icon: str = icon("nodes_default.png")
+    icon: str = icon("nodes_helix.svg")
     op_title: str = "Helix"
     op_category: str = "Curves"
     content_label_objname: str = "fcn_node_bg"
